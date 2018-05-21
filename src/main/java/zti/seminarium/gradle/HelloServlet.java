0d592f94ac59247da00cd67a4468a3c55d063fc7
4kuklewski <4kuklewski@fis.agh.edu.pl>
2018-05-21 23:55:40 +0200

    initial commit

---
+++ src/main/java/zti/seminarium/gradle/HelloServlet.java
@@ -0,0 +1,23 @@
+package zti.seminarium.gradle;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.getWriter().print("Hello, World!");  
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) name = "World";
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response); 
+    }
+}
